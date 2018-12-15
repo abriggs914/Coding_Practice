f936e3caf0a07d403588cd36667efd30dc9e5664
Avery <abriggs1@unb.ca>
2018-12-14 23:22:13 -0400

    2018-12-14 longest word in english language with regex

---
+++ Longest_Word/longestWord.js
@@ -0,0 +1,21 @@
+var fs = require("fs");
+var words = fs.readFileSync("words.txt").toString();
+words = words.split("\n");
+
+// console.log(words[0]);
+
+var badLetters = /[!@#$%^&*()-_'+]/;
+var longestAcceptableWord = "";
+
+for(var testWord of words){
+  if(testWord.length <= longestAcceptableWord.length){
+    continue;
+  }
+  if(testWord.match(badLetters)){
+    continue;
+  }
+  // console.log(testWord);
+  longestAcceptableWord = testWord;
+}
+
+console.log(longestAcceptableWord);
