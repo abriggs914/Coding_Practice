69a84c6b25bf92c3486aa41df349f08f46ab6e01
Avery Briggs <abriggs914@gmail.com>
2019-01-18 20:10:13 -0400

    2019-01-18 Created a course prerequisite web

---
+++ JavaScript/course-web.js
@@ -0,0 +1,155 @@
+let allCourses = [];
+
+const comparePreReqs = (obj1, obj2) => obj1.preReqs.length > obj2.preReqs.length;
+
+const sortCoursesByPreReqs = arr => {
+	//return arr.map(item1 item2 => {return arr.sort(compareTeeth(item['numTeeth']));});
+    return arr.sort(comparePreReqs);
+}
+const getPrevious = (arr) => {
+  if(typeof arr === 'object'){
+    return arr.push(getPrevious(arr.preReqs));
+  }
+  return arr-1;
+  // if(obj.preReqs.length === 0){
+  //   return [];
+  // }
+  // return obj.preReqs;
+}
+
+const makeCourse = (id, courseHours, lst = []) => {
+  let obj = {
+    id: id,
+    ch: courseHours,
+    preReqs: lst,
+  };
+  // correctPreReqs(obj);
+  allCourses.push(obj);
+  return obj;
+}
+
+const print = arr => {
+  for(let i = 0; i < arr.length; i++){
+    // console.log('id: '+ arr[i].id + ', ch: ' + arr[i].ch + ', preReqs: ' + getPrevious(arr[i].preReqs));
+  }
+}
+
+// const mendPreReqs = (obj) => {
+//   let lst = obj.preReqs;
+//   console.log('lst',lst);
+//   if(lst.length === 0){
+//     return [];
+//   }
+//   return lst;
+// }
+//
+// const correctPreReqs = (obj) => {
+//   let lst = obj.preReqs;
+//   console.log(lst);
+//   //console.log(obj);
+//   for(let i = 0; i < lst.length; i++){
+//     lst = mendPreReqs(lst[i]);
+//     console.log('res:',lst);
+//   }
+//   return lst;
+// }
+
+// const correctPreReqs = (obj) => {
+//   let lst = obj.preReqs;
+//   if(lst.length === 0){ // no prerequisits
+//     return [];
+//   }
+//   let res = [];
+//   for(let i = 0; i < lst.length; i++){
+//     res.push(lst[i].preReqs);
+//   }
+//   //     console.log('res:',res);
+//   // console.log('obj: ', obj);
+//   // console.log('lst ', lst);
+//   return res;
+// }
+
+const cycle = (course) => {
+  let lst = course.preReqs;
+  for(let i = 0; i < lst.length; i++){
+
+  }
+}
+
+function handler(course) {
+let n = 0;
+console.log(course);
+  while(course.preReqs.length > n){
+    // console.log('\n\n\n');
+    // console.log(course.preReqs[n]);
+    //   console.log('\n\n\n');
+    if(course.preReqs[n].preReqs.length !== 0){
+      correctPreReqs(course.preReqs[n]);
+    }
+    n++;
+  }
+}
+
+function correctPreReqs(course) {
+  //console.log(typeof course);
+  if(typeof course === 'undefined'){
+    console.log('\t\tundefined');
+    return;
+  }
+  console.log(course);
+  dive = false;
+  //console.log(`{ id: ${course.id}, ch: ${course.ch}, `);
+  console.log();
+  let n = 0
+  //console.log('\t\t\t\t\t\t\t\t\t\thi there\n\n\n\n\n\t');
+  //console.log(course.preReqs);
+  while(course.preReqs.length > n){
+    handler(course.preReqs[n]);
+    n++;
+    //console.log('\t\t\t\t\t\t\t\t\t\thi there\n\n\n\n\n\t');
+  }
+}
+
+let coursesTaken = [makeCourse('BIOL1001', 3),
+		                makeCourse('CHEM1001', 3),
+		                makeCourse('CHEM1006', 2),
+		                makeCourse('MATH1003', 3),
+		                makeCourse('PHYS1061', 3),
+		                makeCourse('PHYS1091', 2),
+		                makeCourse('BIOL1012', 3, [allCourses[0]]),
+		                makeCourse('CHEM1012', 3, [allCourses[1]]),
+                	  makeCourse('CHEM1017', 2, [allCourses[2]]),
+              		  makeCourse('PHYS1092', 2, [allCourses[5]]),
+              		  makeCourse('MATH1013', 3, [allCourses[3]]),
+              		  makeCourse('CHEM2416', 2, [allCourses[8]]),
+		                makeCourse('CHEM2421', 3, [allCourses[7]]),
+		                makeCourse('CHEM2601', 3, [allCourses[7], allCourses[10]]),
+		                makeCourse('ENGL1144', 3),
+                    makeCourse('CS1073',4),
+                    makeCourse('CS1303',4),
+                    makeCourse('CS1203',3),
+                    makeCourse('MATH1503',3),
+                    makeCourse('INFO1103',4,[allCourses[15]]),
+                    makeCourse('CS2333',4, [allCourses[15],allCourses[16]]),
+                    makeCourse('CS1083',4, [allCourses[15]]),
+                    makeCourse('STAT2593',3, [allCourses[10]]),
+                    makeCourse('CS2253',4, [allCourses[16]]),
+                    makeCourse('CS2263',4, [allCourses[21]]),
+                    makeCourse('CS3997',3),
+                    makeCourse('CS3853',4, [allCourses[23]]),
+                    makeCourse('CS2043',4, [allCourses[21]]),
+                    makeCourse('CS2383',4, [allCourses[16], allCourses[21]]),
+                    makeCourse('CS2613',4, [allCourses[21]]),
+                    makeCourse('CS3413',4, [allCourses[24]]),
+                    makeCourse('CS3113',3, [allCourses[15], allCourses[18]]),
+                    makeCourse('CS2063',4, [allCourses[27]]),
+                    makeCourse('CS3383',4, [allCourses[20], allCourses[22], allCourses[28]]),
+                    makeCourse('CS3613',4, [allCourses[20], allCourses[24], allCourses[29]]),
+                    makeCourse('CS3873',4, [allCourses[24]]),
+                    makeCourse('CS3503',4, [allCourses[19]])];
+
+coursesTaken.forEach(obj => {return correctPreReqs(obj);});
+// console.log(allCourses.length);
+// print(allCourses);
+// console.log(allCourses.length);
+// console.log(sortCoursesByPreReqs(coursesTaken));
