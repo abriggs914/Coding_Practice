f4406ec18a04d804bbd07eaf3eabd1cf2d2fbb58
abriggs1 <abriggs1@unb.ca>
2021-03-19 16:47:58 -0300

    2021-03-19 2048 logic

--- Python/TwentyFourtyEight/main.py
+++ Python/TwentyFourtyEight/main.py
@@ -1,4 +1,5 @@
-
+import os
+import time
 import random as rand
 import numpy as np
 from utility import *
@@ -27,6 +28,9 @@ class G2048:
 					res.append((i, j))
 		return res
 		
+	def playable(self):
+		return len(self.find_empty_cells()) > 0
+		
 	def place(self, i, j, v):
 		self.grid[i][j] = v
 		if self.largest_tile is None:
@@ -38,7 +42,7 @@ class G2048:
 					gv = self.grid[p][q] if self.grid[p][q] is not None else 0
 					if max_tile is None or max_tile[2] > gv:
 						max_tile = (p, q, gv)
-			self.max_tile = max_tile
+			self.largest_tile = max_tile
 
 		
 	def gen_random_tile(self):
@@ -59,17 +63,97 @@ class G2048:
 			for row in grid:
 				l = len(row)
 				nr = [c for c in row if c is not None]
+				i = 0
+				while i < len(nr)-1:
+					el = nr[i]
+					if el == nr[i+1]:
+						nr = nr[:i] + [el * 2] + nr[i+2:]
+						i -= 1
+					i += 1
+					
 				row = nr + [None for i in range(l - len(nr))]
 				res.append(row)
 				
 			res = np.transpose(res)
 			return res
-		def shift_down(grid):
-			pass
+			
 		def shift_left(grid):
-			pass
+			# grid = np.transpose(grid)
+			res = []
+			for row in grid:
+				l = len(row)
+				nr = [c for c in row if c is not None]
+				i = 0
+				while i < len(nr)-1:
+					el = nr[i]
+					if el == nr[i+1]:
+						nr = nr[:i] + [el * 2] + nr[i+2:]
+						i -= 1
+					i += 1
+					
+				row = nr + [None for i in range(l - len(nr))]
+				res.append(row)
+				
+			# res = np.transpose(res)
+			return res
+			
+		def shift_down(grid):
+			grid = np.transpose(grid)
+			res = []
+			for row in grid:
+				l = len(row)
+				nr = [c for c in row if c is not None]
+				i = len(nr)-1
+				while i > 0 and len(nr) > 1:
+					# print("\ti:\t\t" + str(i))
+					el = nr[i]
+					if el == nr[i-1]:
+						# print("\tnr B:\t\t" + str(nr))
+						# print("\tnr[:i]:\t\t" + str(nr[:i-1]))
+						# print("\t[el * 2]:\t" + str([el * 2]))
+						# print("\tnr[i+2:]:\t" + str(nr[i+2:]))
+						# nr = nr[i-2:] + [el * 2] + nr[:i-1]
+						# nr = [el * 2] + nr[:i-1]
+						nr = nr[:i-1] + [el * 2] + nr[i+2:]
+						# print("\tnr A:\t\t" + str(nr))
+						i += 1
+					i -= 1
+					
+				row = [None for i in range(l - len(nr))] + nr
+				# print("row: " + str(row))
+				res.append(row)
+				
+			res = np.transpose(res)
+			return res
+			
 		def shift_right(grid):
-			pass
+			# grid = np.transpose(grid)
+			res = []
+			for row in grid:
+				l = len(row)
+				nr = [c for c in row if c is not None]
+				i = len(nr)-1
+				while i > 0 and len(nr) > 1:
+					# print("\ti:\t\t" + str(i))
+					el = nr[i]
+					if el == nr[i-1]:
+						# print("\tnr B:\t\t" + str(nr))
+						# print("\tnr[:i]:\t\t" + str(nr[:i-1]))
+						# print("\t[el * 2]:\t" + str([el * 2]))
+						# print("\tnr[i+2:]:\t" + str(nr[i+2:]))
+						# nr = nr[i-2:] + [el * 2] + nr[:i-1]
+						# nr = [el * 2] + nr[:i-1]
+						nr = nr[:i-1] + [el * 2] + nr[i+2:]
+						# print("\tnr A:\t\t" + str(nr))
+						i += 1
+					i -= 1
+					
+				row = [None for i in range(l - len(nr))] + nr
+				# print("row: " + str(row))
+				res.append(row)
+				
+			# res = np.transpose(res)
+			return res
 
 		# def shift_up(grid):
 		# 	print("shifting up:\n" + str(grid))
@@ -134,12 +218,13 @@ class G2048:
 			self.grid = shift_left(self.grid)
 		elif dir == so["RIGHT"]:
 			self.grid = shift_right(self.grid)
-		print("done global: " + str(self.grid))
+		# print("done global: " + str(self.grid))
 		
 		
 	def __repr__(self):
 		res = "\n"
-		min_width = 2 + len(str(self.max_tile[2]))
+		lt = self.largest_tile[2] if self.largest_tile is not None else 1
+		min_width = 2 + len(str(lt))
 		for row in self.grid:
 			for val in row:
 				res += pad_centre(str(val), min_width) if val is not None else pad_centre("-", min_width)
@@ -147,6 +232,15 @@ class G2048:
 		res += "\n"
 		return res
 		
+def get_move_input():
+	valid = ["W", "A", "S", "D"]
+	legend = ["UP", "LEFT", "DOWN", "RIGHT"]
+	inp = ""
+	while inp.upper() not in valid:
+		inp = input("Up, down, left, or right?")
+	return legend[valid.index(inp.upper())].lower()
+
+clear = lambda: os.system('cls') #on Windows System
 		
 if __name__ == "__main__":
 	
@@ -159,8 +253,19 @@ if __name__ == "__main__":
 	game.place(2, 1, 32)
 	game.place(1, 2, 16)
 	print(game)
-	game.shift_grid(game.shift_options["UP"])
+	# game.shift_grid(game.shift_options["UP"])
 	# game.shift_grid(game.shift_options["DOWN"])
 	# game.shift_grid(game.shift_options["LEFT"])
-	# game.shift_grid(game.shift_options["RIGHT"])
+	game.shift_grid(game.shift_options["RIGHT"])
 	print(game)
+	
+	game = G2048()
+	game.gen_random_tile()
+	game.gen_random_tile()
+	while game.playable():
+		clear()
+		print(game)
+		move_dir = get_move_input()
+		game.shift_grid(move_dir)
+		game.gen_random_tile()
+		time.sleep(0.1)
