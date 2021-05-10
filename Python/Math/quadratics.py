d1089768d09cd9e63795ee01cd9903300867e646
Avery <abriggs1@unb.ca>
2021-05-10 02:39:31 -0300

    2021-05-10 Quadratics

--- Python/Math/quadratics.py
+++ Python/Math/quadratics.py
@@ -14,11 +14,19 @@ def quad(a, b, c, p=None):
 		return quad(a, b, c, True), quad(a, b, c, False)
 		# return (-1 * b) + (((b**2 - (4 * a * c))**0.5) / (2 * a)), (-1 * b) - (((b**2 - (4 * a * c))**0.5) / (2 * a))
 
+def vertex(a, b, c):
+	x = -b / (2 * a)
+	y = (a * x**2) + (b * x) + c
+	return x, y
+
 
 # sol_lw = lambda c, h: ((-4 * inTy * ((-1 * (rate * (1 / inTy))) / (c * h)))**0.5)
-sol_lw = lambda c, h: quad(inTy, 0, ((-1 * rate) * (1 / inTy)) / (c * h), p=True)  #((-4 * inTy * ((-1 * (rate * (1 / inTy))) / (c * h)))**0.5)
+# sol_lw = lambda c, h: quad(inTy, 0, ((-1 * rate) * (1 / inTy)) / (c * h), p=True)  #((-4 * inTy * ((-1 * (rate * (1 / inTy))) / (c * h)))**0.5)
+sol_lw = lambda c, h: quad(inTy * h * rate, 0, -c, p=True)  #((-4 * inTy * ((-1 * (rate * (1 / inTy))) / (c * h)))**0.5)
 
 for c in T_costs:
 	for h in heights:
 		lw = sol_lw(c, h)
-		print("c: {c}, h: {h}, lw: {lw}".format(c=c, h=h, lw=lw))
+		l = (lw**0.5) / inTy
+		w = l
+		print("c: $ {c}, h: {h}\", lw: {lw} y^2, lxw: ({l}\" x {w}\", v: {v})".format(c=c, h=h, lw=lw, l=l, w=w, v=vertex(inTy * h * rate, 0, -c)))
