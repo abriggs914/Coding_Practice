17712f6c8c00b3da52984cd7be9b00e0734e7bf8
abriggs1 <abriggs1@unb.ca>
2021-08-11 16:52:42 -0300

    2021-08-11 TVSeries

--- Python/TV_series/main.py
+++ Python/TV_series/main.py
@@ -2,7 +2,6 @@ import random
 from statistics import mean, median, mode, pstdev, pvariance, stdev, variance
 from tv_series import TVSeries
 
-
 the_office = TVSeries("The Office", {
     1 : 6,
     2 : 22,
