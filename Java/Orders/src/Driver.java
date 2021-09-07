7c48fe5576b0587da7e6724dd78d3b6e0d9c5a8a
Avery <abriggs1@unb.ca>
2021-09-07 00:08:17 -0300

    2021-09-07 Item Chooser

--- Java/Orders/src/Driver.java
+++ Java/Orders/src/Driver.java
@@ -1,10 +1,18 @@
 import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.Random;
 
 public class Driver {
 
+    private static Random rand;
+
+    private static ArrayList<Item> itemsB;
+    private static ArrayList<Item> itemsP;
+    private static ArrayList<Item> itemsS;
+
     public static void main(String[] args) {
         System.out.println("Hello World!");
+        rand = new Random();
 
         Item b_0001 = new ItemB("b_0001", new ArrayList<>(Arrays.asList("blue", "white")));
         Item p_0001 = new ItemP("p_0001", new ArrayList<>(Arrays.asList("white", "pink")));
@@ -17,5 +25,11 @@ public class Driver {
         ));
 
         System.out.println("\n\tItems:\n" + itemsList);
+        System.out.println("\n\trandomItemB:\n" + randomItemB());
+    }
+
+    public static Item randomItemB() {
+        rand.nextInt();
+        return new Item("d", new ArrayList<>());
     }
 }
