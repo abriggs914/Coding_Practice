dfdc5233092a584eb5b6095cbbaf77e9e33e4905
Avery <abriggs1@unb.ca>
2021-11-26 01:56:16 -0400

    2021-11-26 Imperial Graph

--- Python/Imperial Graph/main.py
+++ Python/Imperial Graph/main.py
@@ -1,12 +1,26 @@
 
+
 class Node:
 
     def __init__(self, name):
         self.name = name
+        self.connected_nodes = []
+
+    def add_connection(self, node, cost, dag=True):
+        assert isinstance(node, Node)
+        self.connected_nodes.append((node, cost))
+        if not dag:
+            if self not in [n for n, c in node.connected_nodes]:
+                node.add_connection(self, -cost)
 
     def __repr__(self):
-        return self.name
+        return "Node: \"{}\", BF: {}".format(self.name, len(self.connected_nodes))
 
 
 if __name__ == '__main__':
-    print('PyCharm')
+    n1 = Node("Node 1")
+    n2 = Node("Node 2")
+    n1.add_connection(n2, 3)
+
+    print(n1)
+    print(n2)
