203f55dd25c2c3fc5e1f924807e570c0b12bc991
Avery <abriggs1@unb.ca>
2021-12-10 02:07:21 -0400

    2021-12-10 PCBanking

--- SQL/Transactions Analysis/execs 2.sql
+++ SQL/Transactions Analysis/execs 2.sql
@@ -1,5 +1,8 @@
 EXEC [dbo].[sp_GroupEntities];
-EXEC [dbo].[sp_GroupEntities] @nameIn='amz';
+EXEC [dbo].[sp_GroupEntities] @nameIn='amzn asfjdlij';
+EXEC [dbo].[sp_GroupEntities];
 
 SELECT [dbo].[sv_RemoveNumbers]('amz')
 SELECT [dbo].[sv_RemoveSymbols]('amazon')
+
+EXEC [dbo].sp_CollectEntityData @entity_name='nb liquor', @start_date='2021-12-10', @end_date='2021-12-10'
