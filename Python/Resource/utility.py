b6430b7c5d1d2bb1b665beeb64574feafc00af11
abriggs1 <abriggs1@unb.ca>
2021-12-20 17:04:59 -0400

    2021-12-20 Mobilio

--- Python/Resource/utility.py
+++ Python/Resource/utility.py
@@ -1,3 +1,4 @@
+import datetime
 from locale import currency, setlocale, LC_ALL
 from math import e, ceil, sin, cos, radians
 from random import random, choice, randint
@@ -8,8 +9,8 @@ import os
 
 """
 	General Utility Functions
-	Version..............1.32
-	Date...........2021-12-16
+	Version..............1.33
+	Date...........2021-12-17
 	Author.......Avery Briggs
 """
 
@@ -1474,44 +1475,57 @@ def next_available_file_name(path):
         path = ".".join(spl[:-1]) + " ({}).".format(counter) + spl[-1]
     path.replace("/", "\\")
     return path
-	
+
 
 # leap year calculation: https://www.timeanddate.com/date/leapyear.html
 def random_date(start_year=1, end_year=10000, start_m=None, start_d=None):
-	start_year, end_year = minmax(start_year, end_year)
-	start_year = clamp(1, start_year, end_year)
-	end_year = clamp(start_year + 1, end_year + 1, 10000)
-		
-	r_y = list(range(start_year, end_year))
-	r_m = list(range(1, 13))
-	r_d = list(range(1, 32))
-	r_dsm = list(range(1, 31))
-	r_df = list(range(1, 29))
-	r_dfl = list(range(1, 30))
-	r_sm = [2, 4, 6, 9, 11]
-	y = choice(r_y)
-	m = choice(r_m)
-	if start_m in r_m:
-		m = start_m
-	if m in r_sm:
-		d = choice(r_dsm)
-		if start_d in r_dsm:
-			d = start_d
-	else:
-		d = choice(r_d)
-		if start_d in r_d:
-			d = start_d
-		
-	if m == 2:
-		d = choice(r_df)
-		if start_d in r_df:
-			d = start_d
-		if y % 4 == 0 and (y % 100 != 0 or y % 400 == 0):
-			d = choice(r_dfl)
-			if start_d in r_dfl:
-				d = start_d
-			
-	return "{}-{}-{}".format(("0000" + str(y))[-4:], ("00" + str(m))[-2:], ("00" + str(d))[-2:])
+    start_year, end_year = minmax(start_year, end_year)
+    start_year = clamp(1, start_year, end_year)
+    end_year = clamp(start_year + 1, end_year + 1, 10000)
+
+    r_y = list(range(start_year, end_year))
+    r_m = list(range(1, 13))
+    r_d = list(range(1, 32))
+    r_dsm = list(range(1, 31))
+    r_df = list(range(1, 29))
+    r_dfl = list(range(1, 30))
+    r_sm = [2, 4, 6, 9, 11]
+    y = choice(r_y)
+    m = choice(r_m)
+    if start_m in r_m:
+        m = start_m
+    if m in r_sm:
+        d = choice(r_dsm)
+        if start_d in r_dsm:
+            d = start_d
+    else:
+        d = choice(r_d)
+        if start_d in r_d:
+            d = start_d
+
+    if m == 2:
+        d = choice(r_df)
+        if start_d in r_df:
+            d = start_d
+        if y % 4 == 0 and (y % 100 != 0 or y % 400 == 0):
+            d = choice(r_dfl)
+            if start_d in r_dfl:
+                d = start_d
+
+    return "{}-{}-{}".format(("0000" + str(y))[-4:], ("00" + str(m))[-2:], ("00" + str(d))[-2:])
+
+
+def is_date(date_in, fmt="%Y-%m-%d"):
+    if isinstance(date_in, datetime.datetime) or isinstance(date_in, datetime.date):
+        return True
+    try:
+        d = datetime.datetime.strptime(date_in, fmt)
+        return True
+    except TypeError:
+        print("Can not determine id date param \"{}\" is a valid date using datetime format: {}".format(date_in, fmt))
+    except ValueError:
+        print("Can not determine id date param \"{}\" is a valid date using datetime format: {}".format(date_in, fmt))
+    return False
 
 
 BLK_ONE = "1", "  1  \n  1  \n  1  \n  1  \n  1  "
