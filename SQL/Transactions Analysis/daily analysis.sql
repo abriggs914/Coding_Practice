3efcf847f602fc2a636f02db67ab162811953c21
Avery <abriggs1@unb.ca>
2021-12-22 03:51:39 -0400

    2021-12-22 Transactions

---
+++ SQL/Transactions Analysis/daily analysis.sql
@@ -0,0 +1,70 @@
+
+EXEC [dbo].sp_CollectEntityData @entity_name='bws', @start_date='2015-12-10', @end_date='2025-12-10'
+--EXEC [dbo].sp_CollectEntityData @entity_name='cnb', @start_date='2015-12-10', @end_date='2025-12-10'
+
+DECLARE @data TABLE ([ID] INT IDENTITY(1, 1), [Date] DATETIME, [# Debits] INT, [# Credits] INT, [# Tranasactions] INT, [Total Spent] FLOAT, [Total Earned] FLOAT, [Total] FLOAT)
+
+DECLARE @i INT;
+DECLARE @d AS DATETIME;
+
+SET @d = (SELECT MIN([Date]) FROM [ScotiaTransactions]);
+
+WHILE @d < GETDATE() BEGIN
+	
+	INSERT INTO @data ([Date], [# Debits], [# Credits], [# Tranasactions], [Total Spent], [Total Earned], [Total])
+	SELECT
+		[Date],
+		SUM([# Debits]),
+		SUM([# Credits]),
+		SUM([# Debits]) + SUM([# Credits]),
+		SUM([Total Spent]),
+		SUM([Total Earned]),
+		AVG([Total])
+	FROM (
+		SELECT
+			@d AS [Date],
+			COUNT(*) AS [# Debits],
+			0 AS [# Credits],
+			0 AS [# Tranasactions],
+			SUM([Amount]) AS [Total Spent],
+			0 AS [Total Earned],
+			SUM([Amount]) AS [Total]
+		FROM
+			[ScotiaTransactions]
+		WHERE
+			[Date] BETWEEN @d AND DATEADD(HOUR, 23, DATEADD(MINUTE, 59, DATEADD(SECOND, 59, @d)))
+			AND [Amount] < 0
+		UNION (
+			SELECT
+				@d AS [Date], 
+				0 AS [# Debits],
+				COUNT(*) AS [# Credits],
+				0 AS [# Tranasactions],
+				0 AS [Total Spent],
+				SUM([Amount]) AS [Total Earned],
+				SUM([Amount]) AS [Total]
+			FROM
+				[ScotiaTransactions]
+			WHERE
+				[Date] BETWEEN @d AND DATEADD(HOUR, 23, DATEADD(MINUTE, 59, DATEADD(SECOND, 59, @d)))
+				AND [Amount] >= 0
+		)
+	) AS [Src]
+	GROUP BY
+		[Date]
+
+	SET @d = DATEADD(DAY, 1, @d);
+
+END
+
+SELECT * FROM @data
+SELECT
+	SUM([# Credits]) AS [# Credits],
+	SUM([# Debits]) AS [# Debits],
+	SUM([# Tranasactions]) AS [# Transactions],
+	SUM([Total Spent]) AS [Total Spent],
+	SUM([Total Earned]) AS [Total Earned],
+	SUM([Total]) AS [Total]
+FROM
+	@data
+SELECT COUNT(*) AS [# Non-Transaction Days] FROM @data WHERE [Total] IS NULL
