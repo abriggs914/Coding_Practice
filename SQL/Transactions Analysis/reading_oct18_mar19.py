b30ec8d27831c31721151d72ab6b17b26c24d20d
Avery <abriggs1@unb.ca>
2022-02-02 02:41:08 -0400

    2022-02-02 Transactions

--- SQL/Transactions Analysis/reading_oct18_mar19.py
+++ SQL/Transactions Analysis/reading_oct18_mar19.py
@@ -1,6 +1,7 @@
 import csv
 
-with open("oct 2018 - mar 2019.csv", "r") as f:
+#  with open("oct 2018 - mar 2019.csv", "r") as f:
+with open("2022-02-02.csv", "r") as f:
 	d = csv.DictReader(f)
 	for dtt in d:
 		dd = dtt["Transaction Date"]
@@ -8,5 +9,6 @@ with open("oct 2018 - mar 2019.csv", "r") as f:
 		dn = dtt["Notes"]
 		dt = dtt["Transaction Type"]
 		de = dtt["Entity"]
-		print("('{}', {}, '{}', '{}', '{}', NULL),".format(dd, da, dn, dt, de))
+		di = dtt["TransactionTypeID"]
+		print("('{}', {}, '{}', '{}', '{}', {}),".format(dd, da, dn, dt, de, di))
 	
