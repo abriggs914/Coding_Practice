656c9e7d73860bc55185f635646b135b8682c2ce
Avery Briggs <abriggs1@unb.ca>
2022-07-29 17:02:55 -0300

    2022-07-29 Bad Widgetes

---
+++ SQL/Trigger Skeleton.sql
@@ -0,0 +1,54 @@
+-- ================================================
+-- Template generated from Template Explorer using:
+-- Create Trigger (New Menu).SQL
+--
+-- Use the Specify Values for Template Parameters 
+-- command (Ctrl-Shift-M) to fill in the parameter 
+-- values below.
+--
+-- See additional Create Trigger templates for more
+-- examples of different Trigger statements.
+--
+-- This block of comments will not be included in
+-- the definition of the function.
+-- ================================================
+SET ANSI_NULLS ON
+GO
+SET QUOTED_IDENTIFIER ON
+GO
+-- =============================================
+-- Author:		<Author,,Name>
+-- Create date: <Create Date,,>
+-- Description:	<Description,,>
+-- =============================================
+CREATE TRIGGER ##### [dbo].[tr_ITDUpdateDates]
+   ON  [ITD InvMaster]
+   AFTER INSERT, DELETE, UPDATE #####
+AS 
+BEGIN
+	-- SET NOCOUNT ON added to prevent extra result sets from
+	-- interfering with SELECT statements.
+	SET NOCOUNT ON;
+
+	IF TRIGGER_NESTLEVEL() < 2 BEGIN
+	
+		DECLARE @user NVARCHAR(20);
+		DECLARE @activity NVARCHAR(20);
+
+		-- Insert statements for trigger here
+		IF EXISTS (SELECT * FROM inserted) AND EXISTS (SELECT * FROM deleted) BEGIN
+			SET @activity = 'UPDATE';
+			SET @user = SYSTEM_USER;
+		END
+		IF EXISTS (SELECT * FROM inserted) AND NOT EXISTS (SELECT * FROM deleted) BEGIN
+			SET @activity = 'INSERT';
+			SET @user = SYSTEM_USER;
+		END
+		IF EXISTS (SELECT * FROM deleted) AND NOT EXISTS (SELECT * FROM inserted) BEGIN 
+			SET @activity = 'DELETE';
+			SET @user = SYSTEM_USER;
+		END
+
+	END
+END
+GO
