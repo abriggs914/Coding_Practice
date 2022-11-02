3aebebfa68d7d1cac8aaa2cc2ced4d1d893270c3
Avery Briggs <abriggs1@unb.ca>
2022-11-02 17:13:00 -0300

    2022-11-02 pyodbc_treeview_demo

---
+++ Python/Resource/pyodbc_treeview_demo.py
@@ -0,0 +1,18 @@
+
+if __name__ == '__main__':
+
+
+    from pyodbc_connection import *
+    from tkinter_utility import *
+
+    WIN = tkinter.Tk()
+    WIN.geometry(f"800x600")
+    WIN.title("Pyodbc + Treeview")
+    df_IT_requests = connect("SELECT * FROM [IT Requests]")
+
+    tv_label_IT_requests, label_IT_requests, treeview_IT_requests, scroll_x_IT_requests, scroll_y_IT_requests = treeview_factory(WIN, df_IT_requests)
+    label_IT_requests.grid()
+    scroll_x_IT_requests.grid()
+    treeview_IT_requests.grid()
+    scroll_y_IT_requests.grid(column=1)
+    WIN.mainloop()
