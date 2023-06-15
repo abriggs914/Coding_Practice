47fba4ca19710c3a7c6b4c2bbf1f594761d5669e
Avery <abriggs1@unb.ca>
2023-06-15 11:15:14 -0300

    2023-06-15 HockeyPool

---
+++ Python/DataVisualizer/gpt_nhl_team_colour_schemes.py
@@ -0,0 +1,195 @@
+scheme = {
+    "Anaheim Ducks": [
+        "Orange: #FE7F00",
+        "Gold: #B5985A",
+        "Black: #111111",
+        "White: #FFFFFF"
+    ],
+    "Arizona Coyotes": [
+        "Sedona Red: #8C2633",
+        "Desert Sand: #C09867",
+        "Black: #111111",
+        "White: #FFFFFF"
+    ],
+    "Boston Bruins": [
+        "Gold: #FFB81C",
+        "Black: #000000",
+        "White: #FFFFFF"
+    ],
+    "Buffalo Sabres": [
+        "Navy Blue: #041E42",
+        "Gold: #FCB514",
+        "Silver: #ADAFAA",
+        "White: #FFFFFF"
+    ],
+    "Calgary Flames": [
+        "Red: #C8102E",
+        "Gold: #FFC758",
+        "Black: #111111",
+        "White: #FFFFFF",
+        "Silver: #B2B7BB"
+    ],
+    "Carolina Hurricanes": [
+        "Red: #CC0000",
+        "Black: #000000",
+        "White: #FFFFFF",
+        "Gray: #A2AAAD"
+    ],
+    "Chicago Blackhawks": [
+        "Red: #CF0A2C",
+        "Black: #000000",
+        "White: #FFFFFF"
+    ],
+    "Colorado Avalanche": [
+        "Burgundy: #6F263D",
+        "Steel Blue: #236192",
+        "Black: #000000",
+        "White: #FFFFFF"
+    ],
+    "Columbus Blue Jackets": [
+        "Union Blue: #00285C",
+        "Goal Red: #CE1126",
+        "Silver: #A4A9AD",
+        "White: #FFFFFF"
+    ],
+    "Dallas Stars": [
+        "Victory Green: #006847",
+        "Black: #111111",
+        "White: #FFFFFF",
+        "Silver: #C0C0C0"
+    ],
+    "Detroit Red Wings": [
+        "Red: #CE1126",
+        "White: #FFFFFF"
+    ],
+    "Edmonton Oilers": [
+        "Orange: #FF4C00",
+        "Blue: #041E42",
+        "White: #FFFFFF"
+    ],
+    "Florida Panthers": [
+        "Panther Red: #C8102E",
+        "Panther Blue: #002E5F",
+        "Jet Black: #000000",
+        "White: #FFFFFF",
+        "Tan: #B9975B"
+    ],
+    "Los Angeles Kings": [
+        "Forum Blue: #003E7E",
+        "Gold: #AFB7BA",
+        "White: #FFFFFF",
+        "Black: #111111"
+    ],
+    "Minnesota Wild": [
+        "Forest Green: #026F5F",
+        "Iron Range Red: #B8204F",
+        "Harvest Gold: #FFB81C",
+        "White: #FFFFFF"
+    ],
+    "Montreal Canadiens": [
+        "Red: #AF1E2D",
+        "Blue: #192168",
+        "White: #FFFFFF"
+    ],
+    "Nashville Predators": [
+        "Gold: #FFB81C",
+        "Navy Blue: #041E42",
+        "White: #FFFFFF"
+    ],
+    "New Jersey Devils": [
+        "Red: #CE1126",
+        "Black: #000000",
+        "White: #FFFFFF"
+    ],
+    "New York Islanders": [
+        "Royal Blue: #00539B",
+        "Orange: #F57D31",
+        "White: #FFFFFF",
+        "Gray: #8D9093"
+    ],
+    "New York Rangers": [
+        "Blue: #0038A8",
+        "Red: #CE1126",
+        "White: #FFFFFF"
+    ],
+    "Ottawa Senators": [
+        "Red: #C52032",
+        "Black: #000000",
+        "White: #FFFFFF",
+        "Gold: #FFCD00"
+    ],
+    "Philadelphia Flyers": [
+        "Orange: #F74902",
+        "Black: #000000",
+        "White: #FFFFFF"
+    ],
+    "Pittsburgh Penguins": [
+        "Gold: #FFB81C",
+        "Black: #000000",
+        "White: #FFFFFF"
+    ],
+    "San Jose Sharks": [
+        "Teal: #006D75",
+        "Orange: #EA7200",
+        "Black: #000000",
+        "White: #FFFFFF"
+    ],
+    "Seattle Kraken": [
+        "Navy Blue: #001628",
+        "Light Blue: #32CCD6",
+        "Ice Blue: #75D6FF",
+        "Red: #FF2D55",
+        "White: #FFFFFF",
+        "Silver: #8E9090"
+    ],
+    "St. Louis Blues": [
+        "Blue: #002F87",
+        "Gold: #FCB514",
+        "Navy: #041E42",
+        "White: #FFFFFF"
+    ],
+    "Tampa Bay Lightning": [
+        "Blue: #002868",
+        "Black: #111111",
+        "White: #FFFFFF",
+        "Gray: #A2AAAD"
+    ],
+    "Toronto Maple Leafs": [
+        "Blue: #003E7E",
+        "White: #FFFFFF",
+        "Navy Blue: #00205B"
+    ],
+    "Vancouver Canucks": [
+        "Blue: #001F5B",
+        "Green: #00843D",
+        "Silver: #9E9E9E",
+        "White: #FFFFFF"
+    ],
+    "Vegas Golden Knights": [
+        "Steel Gray: #B4975A",
+        "Gold: #FFC72C",
+        "Red: #C8102E",
+        "Black: #000000",
+        "White: #FFFFFF"
+    ],
+    "Washington Capitals": [
+        "Red: #C8102E",
+        "Navy Blue: #041E42",
+        "White: #FFFFFF",
+        "Silver: #C0C0C0"
+    ],
+    "Winnipeg Jets": [
+        "Navy Blue: #041E42",
+        "Aviator Blue: #004C97",
+        "Polar Night Blue: #92C1E9",
+        "White: #FFFFFF"
+    ]
+}
+
+if __name__ == '__main__':
+    y = ['Anaheim Ducks', '\nOrange: #FE7F00\nGold: #B5985A\nBlack: #111111\nWhite: #FFFFFF\nArizona Coyotes', '\nSedona Red: #8C2633\nDesert Sand: #C09867\nBlack: #111111\nWhite: #FFFFFF\nBoston Bruins', '\nGold: #FFB81C\nBlack: #000000\nWhite: #FFFFFF\nBuffalo Sabres', '\nNavy Blue: #041E42\nGold: #FCB514\nSilver: #ADAFAA\nWhite: #FFFFFF\nCalgary Flames', '\nRed: #C8102E\nGold: #FFC758\nBlack: #111111\nWhite: #FFFFFF\nSilver: #B2B7BB\nCarolina Hurricanes', '\nRed: #CC0000\nBlack: #000000\nWhite: #FFFFFF\nGray: #A2AAAD\nChicago Blackhawks', '\nRed: #CF0A2C\nBlack: #000000\nWhite: #FFFFFF\nColorado Avalanche', '\nBurgundy: #6F263D\nSteel Blue: #236192\nBlack: #000000\nWhite: #FFFFFF\nColumbus Blue Jackets', '\nUnion Blue: #00285C\nGoal Red: #CE1126\nSilver: #A4A9AD\nWhite: #FFFFFF\nDallas Stars', '\nVictory Green: #006847\nBlack: #111111\nWhite: #FFFFFF\nSilver: #C0C0C0\nDetroit Red Wings', '\nRed: #CE1126\nWhite: #FFFFFF\nEdmonton Oilers', '\nOrange: #FF4C00\nBlue: #041E42\nWhite: #FFFFFF\nFlorida Panthers', '\nPanther Red: #C8102E\nPanther Blue: #002E5F\nJet Black: #000000\nWhite: #FFFFFF\nTan: #B9975B\nLos Angeles Kings', '\nForum Blue: #003E7E\nGold: #AFB7BA\nWhite: #FFFFFF\nBlack: #111111\nMinnesota Wild', '\nForest Green: #026F5F\nIron Range Red: #B8204F\nHarvest Gold: #FFB81C\nWhite: #FFFFFF\nMontreal Canadiens', '\nRed: #AF1E2D\nBlue: #192168\nWhite: #FFFFFF\nNashville Predators', '\nGold: #FFB81C\nNavy Blue: #041E42\nWhite: #FFFFFF\nNew Jersey Devils', '\nRed: #CE1126\nBlack: #000000\nWhite: #FFFFFF\nNew York Islanders', '\nRoyal Blue: #00539B\nOrange: #F57D31\nWhite: #FFFFFF\nGray: #8D9093\nNew York Rangers', '\nBlue: #0038A8\nRed: #CE1126\nWhite: #FFFFFF\nOttawa Senators', '\nRed: #C52032\nBlack: #000000\nWhite: #FFFFFF\nGold: #FFCD00\nPhiladelphia Flyers', '\nOrange: #F74902\nBlack: #000000\nWhite: #FFFFFF\nPittsburgh Penguins', '\nGold: #FFB81C\nBlack: #000000\nWhite: #FFFFFF\nSan Jose Sharks', '\nTeal: #006D75\nOrange: #EA7200\nBlack: #000000\nWhite: #FFFFFF\nSeattle Kraken', '\nNavy Blue: #001628\nLight Blue: #32CCD6\nIce Blue: #75D6FF\nRed: #FF2D55\nWhite: #FFFFFF\nSilver: #8E9090\nSt. Louis Blues', '\nBlue: #002F87\nGold: #FCB514\nNavy: #041E42\nWhite: #FFFFFF\nTampa Bay Lightning', '\nBlue: #002868\nBlack: #111111\nWhite: #FFFFFF\nGray: #A2AAAD\nToronto Maple Leafs', '\nBlue: #003E7E\nWhite: #FFFFFF\nNavy Blue: #00205B\nVancouver Canucks', '\nBlue: #001F5B\nGreen: #00843D\nSilver: #9E9E9E\nWhite: #FFFFFF\nVegas Golden Knights', '\nSteel Gray: #B4975A\nGold: #FFC72C\nRed: #C8102E\nBlack: #000000\nWhite: #FFFFFF\nWashington Capitals', '\nRed: #C8102E\nNavy Blue: #041E42\nWhite: #FFFFFF\nSilver: #C0C0C0\nWinnipeg Jets', '\nNavy Blue: #041E42\nAviator Blue: #004C97\nPolar Night Blue: #92C1E9\nWhite: #FFFFFF\n']
+    z = [(y[i].split("\n")[-1], "\n".join(y[i + 1].split("\n")[:-1])) for i in range(len(y) - 1)]
+    a = [[t] + [cs for cs in list(c.rsplit("\n")) if cs] for t, c in z]
+    b = {a_[0]: a_[1:] for a_ in a}
+    print("{\t\"" + "\n\t],\n\t".join(
+        ["\"" + k + "\": [\n\t\t\"" + "\",\n\t\t\"".join(v) + "\"" for k, v, in b.items()]) + "\n\t]\n}")
