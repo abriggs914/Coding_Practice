f8c81d556ed8e27f58884e6d5c7f128b556f207e
Avery <abriggs1@unb.ca>
2024-01-29 11:07:34 -0400

    2024-01-29 Hockey Pool

--- Python/NHL Team Games/version_2024_01_24.py
+++ Python/NHL Team Games/version_2024_01_24.py
@@ -2,6 +2,103 @@ import datetime
 import random
 
 
+def select_remaining_games(data: dict) -> dict:
+	w_data = {k: ([v_ for v_ in v] if hasattr(v, "__iter__") else v) for k, v in data.items()}
+	print(f"{w_data=}")
+
+	def brute_selector(lst, max_seq_tries=10, max_reruns=100):
+
+		if max_reruns <= 0:
+			raise ValueError("Error cannot rerun this anymore.")
+
+		w_lst = [v for v in lst]
+		choices = set()
+		last_tried = [None, 0]
+		failure = ""
+		while w_lst:
+			a = random.choice(w_lst)
+			len_ = len(w_lst)
+			w_lst.remove(a)
+			# print(f"{a=}")
+			if len_ >= 2:
+				b = random.choice(w_lst)
+				tup = a, b
+				if a != b and (tup not in choices):
+					choices.add(tup)
+					w_lst.remove(b)
+				else:
+					if last_tried[0] == tup:
+						last_tried[1] += 1
+					else:
+						last_tried = [tup, 1]
+
+					w_lst.append(a)
+
+				if last_tried[1] >= max_seq_tries:
+					failure = f"times exceeded trying {tup=}"
+					print(f"{max_reruns=}, {tup=}, {failure=}, {w_lst=}, lc={len(choices)}, {choices=}")
+
+				# print(f"{w_lst=}")
+
+			else:
+				failure = f"List too small {len_=}"
+				print(f"{max_reruns=}, {a=}, {failure=}, {w_lst=}, lc={len(choices)}, {choices=}")
+
+			if failure:
+				return brute_selector(lst, max_seq_tries=max_seq_tries, max_reruns=max_reruns-1)
+
+		return list(choices)
+
+	c_data = {}
+	for k, v in w_data.items():
+		c_data[k] = brute_selector(v)
+
+	return c_data
+	# try:
+	# except ValueError:
+
+
+
+	# # for d in divs[same_div]:
+	# #	lo = [t for t in d for i in range(5)]
+	# #	for t1 in d:
+	# cho = set()
+	# i = 0
+	# while (len(cho) < 5) and inter_divs_left_over[same_div]:
+	# 	i += 1
+	# 	# ct = random.choice(inter_divs_left_over[same_div])
+	# 	print(f"{([t1] + list(cho))=}")
+	# 	print(f"{inter_divs_left_over[same_div][0]=}, {inter_divs_left_over[same_div]=}")
+	# 	print(f"{[t2 for t2 in inter_divs_left_over[same_div] if t2 not in ([t1] + list(cho))]=}")
+	# 	# ct = inter_divs_left_over[same_div][0] if (t1 != inter_divs_left_over[same_div][0]) else [t2 for t2 in inter_divs_left_over[same_div] if t2 not in ([t1] + list(cho))][0]
+	# 	ct = [t2 for t2 in inter_divs_left_over[same_div] if t2 not in ([t1] + list(cho))][0]
+	# 	print(f"{t1=}, {ct=}")
+	# 	if (ct != t1) and (ct not in cho):
+	# 		# min_t, max_t = inter_divs_counts[same_div][inter_divs_left_over[same_div][0]], inter_divs_counts[same_div][inter_divs_left_over[same_div][-1]]
+	# 		# print(f"{min_t=}, {max_t=}, {inter_divs_left_over[same_div]=}")
+	# 		# if abs(min_t - max_t) > 1:
+	# 		# 	ct = inter_divs_left_over[same_div][0] if (t1 != inter_divs_left_over[same_div][0]) else [t2 for t2 in inter_divs_left_over[same_div] if t2 not in [[t1] + list(cho)]][0]
+	# 		print(f"{i=}")
+	# 		cho.add(ct)
+	# 		inter_divs_left_over[same_div].remove(ct)
+	# 		inter_divs_left_over[same_div].sort(key=lambda d: inter_divs_counts[same_div][d])
+	# 	curr_time = datetime.datetime.now()
+	#
+	# 	if (curr_time - start_time).total_seconds() > total_allowed_time:
+	# 		raise ValueError(
+	# 			f"Timeout\n\t{i=}\n\t{t1=}\n\t{same_div=}\n\t{cho=}\n\t{inter_divs_left_over[same_div]=}\n\t{inter_divs_left_over=}\n\t{sched=}\n\t{inter_divs_counts[same_div]=}\n\t{inter_divs_counts=}")
+	#
+	# # inter_divs_left_over[same_div].remove(t1)
+	# sched[t1] += list(cho)
+	# # inter_divs_counts[same_div][t1] += (5 / 2)
+	# for t2 in cho:
+	# 	inter_divs_counts[same_div][t2] += 1  # (1 / 2)
+	# print(f"INTER-DIV {t1=}, div={same_div}, {cho=}")
+	#
+	# print(f"{inter_divs_left_over=}")
+
+
+
 if __name__ == "__main__":
 
 	teams = list(map(lambda x: chr(x), range(97, 97+26)))
@@ -11,6 +108,7 @@ if __name__ == "__main__":
 	confs = {"e": ["a", "m"], "w": ["c", "p"]}
 
 	sched = {}
+	games = []
 	
 	inter_divs_left_over = {d: [t for t in d_teams for i in range(5)] for d, d_teams in divs.items()}
 	#inter_divs_counts = {d: {t: 0 for t in d_teams for i in range(8)} for d, d_teams in divs.items()}
@@ -21,6 +119,8 @@ if __name__ == "__main__":
 	total_allowed_time = 10
 	start_time = datetime.datetime.now()
 	curr_time = datetime.datetime.now()
+
+	jj = [0, 0, 0]
 	
 	for t1 in teams:
 		same_div = [k for k, v in divs.items() if t1 in v][0]		
@@ -38,14 +138,18 @@ if __name__ == "__main__":
 			for t2 in teams:
 				if t1 != t2:
 					sched[t1].append(t2)
+					jj[0] += 1
+
+		print(f"A {jj=}")
 
 		# add 1 game for each inter-conference team.
 		# 15 other teams * 1 = 15 games | 77 games Total
 		sched[t1] += [t for t in [[t for t in [c1 + c2]] for c1, c2 in [[t for t in [divs[d_] for d_ in d]] for c, d in confs.items() if (c == "e")]][0][0] if t != t1]
+		jj[1] += 15
+		print(f"B {jj=}")
 		
 		# add 1 game for 5 inter-division teams
 		# 5 other games * 1 = 5 games | 82 games total
-		
 
 		v2024_01_24_2033 = """
 		#for d in divs[same_div]:
@@ -86,6 +190,20 @@ if __name__ == "__main__":
 		print(f"{inter_divs_left_over=}")
 		
 	"""
-		
+
+	remaining_games = select_remaining_games(inter_divs_left_over)
+	print(f"AAA")
+	for div, games_ in remaining_games.items():
+		for t1_t2 in games_:
+			t1, t2 = t1_t2
+			games.append(t1_t2)
+			sched[t1].append(t2)
+			sched[t2].append(t1)
+			jj[2] += 2
+
 	print(f"{len(sched['a'])=}")
 	print(f"{sched=}")
+	print(f"{remaining_games=}")
+	print(f"{len(games)=}")
+	print(f"{games=}")
+	print(f"{jj=}")
--- Python/Resource/nhl_api_utility.py
+++ Python/Resource/nhl_api_utility.py
@@ -174,6 +174,188 @@ class NHLAPIHandler:
         return self.query_url(url)
 
 
+def playoff_pool_sheet(
+        n_forwards: int = 7,
+        n_defence: int = 3
+):
+    nhl_api = NHLAPIHandler()
+    today = datetime.datetime.today()
+    standings_today = nhl_api.get_standings(today)
+    print(f"{standings_today=}")
+    standings_data = {}
+
+    top_east = []
+    top_west = []
+    top_atl = []
+    top_met = []
+    top_cen = []
+    top_pac = []
+    wc_east = []
+    wc_west = []
+
+    # w = '/stats/rest/en/skater/summary', h = 'http://api.nhle.com', t = '/stats/rest/en/leaders/skaters/goals', HOST_NAME = 'https://api-web.nhle.com'
+
+    for standings_data_ in standings_today["standings"]:
+        conf_abbrev = standings_data_.get("conferenceAbbrev")
+        conf_name = standings_data_.get("conferenceName")
+        div_abbrev = standings_data_.get("divisionAbbrev")
+        div_name = standings_data_.get("divisionName")
+
+        league_place = standings_data_.get("leagueSequence")
+        conf_place = standings_data_.get("conferenceSequence")
+        div_place = standings_data_.get("divisionSequence")
+
+        league_place_home = standings_data_.get("leagueHomeSequence")
+        league_place_road = standings_data_.get("leagueRoadSequence")
+        league_place_l10 = standings_data_.get("leagueL10Sequence")
+
+        conf_place_home = standings_data_.get("conferenceHomeSequence")
+        div_place_home = standings_data_.get("divisionHomeSequence")
+        conf_place_road = standings_data_.get("conferenceRoadSequence")
+        div_place_road = standings_data_.get("divisionRoadSequence")
+        conf_place_l10 = standings_data_.get("conferenceL10Sequence")
+        div_place_l10 = standings_data_.get("divisionL10Sequence")
+        wc_place = standings_data_.get("wildcardSequence")
+
+        team_name = standings_data_.get("teamName", {}).get("default")
+        team_name_common = standings_data_.get("teamCommonName", {}).get("default")
+        team_name_abbrev = standings_data_.get("teamAbbrev", {}).get("default")
+        team_name_fr = standings_data_.get("teamName", {}).get("fr")
+        team_name_common_fr = standings_data_.get("teamCommonName", {}).get("fr")
+        team_name_abbrev_fr = standings_data_.get("teamAbbrev", {}).get("fr")
+        team_logo = standings_data_.get("teamLogo")
+
+        streak_count = standings_data_.get("streakCount", 0)
+        streak_code = standings_data_.get("streakCode")
+        games_played = standings_data_.get("gamesPlayed", 0)
+        points = standings_data_.get("points", 0)
+        win_pctg = standings_data_.get("winPctg", 0)
+        wins = standings_data_.get("wins", 0)
+        losses = standings_data_.get("losses", 0)
+        wins_ot = standings_data_.get("shootoutWins", 0)
+        losses_ot = standings_data_.get("otLosses", 0)
+        losses_so = standings_data_.get("shootoutLosses", 0)
+        wins_reg = standings_data_.get("regulationWins", 0)
+        wins_reg_ot = standings_data_.get("regulationPlusOtWins", 0)
+        wins_reg_ot_pctg = standings_data_.get("regulationPlusOtWinsPctg", 0)
+
+        point_pctg = standings_data_.get("pointsPctg", 0)
+        goal_diff = standings_data_.get("goalDifferential", 0)
+        goal_diff_pctg = standings_data_.get("goalDifferentialPctg", 0)
+        goals_for = standings_data_.get("goalsFor", 0)
+        goals_against = standings_data_.get("goalsAgainst", 0)
+        goals_per_game = standings_data_.get("goalsForPctg", 0)
+
+        games_played_home = standings_data_.get("homeGamesPlayed", 0)
+        goal_diff_home = standings_data_.get("homeGoalDifferential", 0)
+        goals_against_home = standings_data_.get("homeGoalsAgainst", 0)
+        goals_for_home = standings_data_.get("homeGoalsFor", 0)
+        losses_home = standings_data_.get("homeLosses", 0)
+        ot_losses_home = standings_data_.get("homeOtLosses", 0)
+        wins_home = standings_data_.get("homeWins", 0)
+        points_home = standings_data_.get("homePoints", 0)
+        wins_reg_ot_home = standings_data_.get("homeRegulationPlusOtWins", 0)
+        wins_reg_home = standings_data_.get("homeRegulationWins", 0)
+
+        games_played_l10 = standings_data_.get("l10GamesPlayed", 0)
+        goal_diff_l10 = standings_data_.get("l10GoalDifferential", 0)
+        goals_against_l10 = standings_data_.get("l10GoalsAgainst", 0)
+        goals_for_l10 = standings_data_.get("l10GoalsFor", 0)
+        losses_l10 = standings_data_.get("l10Losses", 0)
+        ot_losses_l10 = standings_data_.get("l10OtLosses", 0)
+        wins_l10 = standings_data_.get("l10Wins", 0)
+        points_l10 = standings_data_.get("l10Points", 0)
+        wins_reg_ot_l10 = standings_data_.get("l10RegulationPlusOtWins", 0)
+        wins_reg_l10 = standings_data_.get("l10RegulationWins", 0)
+
+        games_played_road = standings_data_.get("roadGamesPlayed", 0)
+        goal_diff_road = standings_data_.get("roadGoalDifferential", 0)
+        goals_against_road = standings_data_.get("roadGoalsAgainst", 0)
+        goals_for_road = standings_data_.get("roadGoalsFor", 0)
+        losses_road = standings_data_.get("roadLosses", 0)
+        ot_losses_road = standings_data_.get("roadOtLosses", 0)
+        wins_road = standings_data_.get("roadWins", 0)
+        points_road = standings_data_.get("roadPoints", 0)
+        wins_reg_ot_road = standings_data_.get("roadRegulationPlusOtWins", 0)
+        wins_reg_road = standings_data_.get("roadRegulationWins", 0)
+
+        if conf_abbrev == "W":
+            top_west.insert(conf_place, standings_data_)
+            if div_abbrev == "P":
+                top_pac.insert(div_place, standings_data_)
+            else:
+                top_cen.insert(div_place, standings_data_)
+        else:
+            top_east.insert(conf_place, standings_data_)
+            if div_abbrev == "A":
+                top_atl.insert(div_place, standings_data_)
+            else:
+                top_met.insert(div_place, standings_data_)
+
+    top_atl = top_atl[:3]
+    top_met = top_met[:3]
+    top_cen = top_cen[:3]
+    top_pac = top_pac[:3]
+
+    for team_data in top_east:
+        div = team_data["divisionAbbrev"]
+        if div == "A":
+            if team_data in top_atl:
+                continue
+            else:
+                wc_east.append(team_data)
+        else:
+            if team_data in top_met:
+                continue
+            else:
+                wc_east.append(team_data)
+
+        if len(wc_east) == 2:
+            break
+
+    for team_data in top_west:
+        div = team_data["divisionAbbrev"]
+        if div == "P":
+            if team_data in top_pac:
+                continue
+            else:
+                wc_west.append(team_data)
+        else:
+            if team_data in top_cen:
+                continue
+            else:
+                wc_west.append(team_data)
+
+        if len(wc_west) == 2:
+            break
+
+    print(f"\nTop East")
+    print(f"\nTop Atlantic:")
+    for team_data in top_atl:
+        print(f"{team_data['teamCommonName']['default']}")
+    print(f"\nTop Metropolitan:")
+    for team_data in top_met:
+        print(f"{team_data['teamCommonName']['default']}")
+
+    print(f"\nWildcard East:")
+    for team_data in wc_east:
+        print(f"{team_data['teamCommonName']['default']}")
+
+    print(f"\nTop West:")
+    print(f"\nTop Central:")
+    for team_data in top_cen:
+        print(f"{team_data['teamCommonName']['default']}")
+    print(f"\nTop Pacific:")
+    for team_data in top_pac:
+        print(f"{team_data['teamCommonName']['default']}")
+
+    print(f"\nWildcard West:")
+    for team_data in wc_west:
+        print(f"{team_data['teamCommonName']['default']}")
+
+    print(f"{nhl_api.get_schedule(today)}")
+
+
 if __name__ == "__main__":
     nhl_api = NHLAPIHandler()
 
@@ -189,3 +371,7 @@ if __name__ == "__main__":
     df_scores_today_oddsPartners = pd.DataFrame(scores_today.get("oddsPartners", {}))
     scores_today_dict = scores_today.get("games", [])
     df_scores_today_games = pd.DataFrame(scores_today_dict)
+
+    print(f"{nhl_api.get_standings(today)=}")
+
+    playoff_pool_sheet()
