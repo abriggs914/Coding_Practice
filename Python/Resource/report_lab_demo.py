76891d70d7024c282ac76005cdb1b3b99ff643d2
Avery <abriggs1@unb.ca>
2024-02-20 02:06:26 -0400

    2024-02-20 Hockey Pool

--- Python/Resource/report_lab_demo.py
+++ Python/Resource/report_lab_demo.py
@@ -229,11 +229,12 @@ if __name__ == '__main__':
                 row = i // 4
                 x = table_x + col * col_width
                 y = table_y - row * row_height
+                print(f"{row_key=}, {item=}")
                 canvas.drawString(x + checkbox_size + 5, y - 3, item)
                 canvas.rect(x, y - checkbox_size / 2, checkbox_size, checkbox_size, stroke=1, fill=0)
                 box_row_count[row_key] += 1
                 box_row_count[row_key] %= box_row_max[row_key]
-                print(f"{row_key=}, {box_row_count=}")
+                # print(f"{row_key=}, {box_row_count=}")
 
             # Save the PDF
             canvas.save()
@@ -257,47 +258,113 @@ if __name__ == '__main__':
         # Generate the PDF
         # generate_pdf(data, title)
 
+        n_forward_boxes = 7
+        n_defence_boxes = 3
+        n_forwards_per_boxes = 6
+        n_defence_per_boxes = 6
+        n_teams_per_box = 8
+
         pool_boxes = nhl_api_utility.playoff_pool_sheet_view_only(
-            kwargs_nhl_api_utility={"view_only": False},
+            n_forward_boxes=n_forward_boxes,
+            n_defence_boxes=n_defence_boxes,
+            n_forwards_per_boxes=n_forwards_per_boxes,
+            n_defence_per_boxes=n_defence_per_boxes,
+            kwargs_nhl_api_utility={
+                "view_only": False,
+                "max_query_hold_time": 60*60*24*2
+            },
             do_save_api_handler=True,
             pool_texts=True
         )
         print(f"{pool_boxes=}")
 
-        prepped_pool_boxes = []
+        # prepped_pool_boxes = [[None]*4]*((8*6) + (1*5))
+        # prepped_pool_boxes = [[None]*4]*(8*6)
+        prepped_pool_boxes = [[None for j in range(4)] for i in range(8*6)]
+        print(f"\n\n\tSTART\n{len(prepped_pool_boxes)=}, {prepped_pool_boxes=}")
 
+        ordered_players = []
+        cids = [0, 0, 0, 0]
         for conf, conf_data in pool_boxes.items():
             for position, position_data in conf_data.items():
                 for i, box in enumerate(position_data):
+                    # print(f"== {i=}, box={str(box)[:25]}")
+                    col_idx = i % 4
                     for j, text in enumerate(box):
-                        print(f"\t{i=}, {j=}, {((j + 1)*4)+i=}, {(i*6)+j=}, {text=}")
-                        prepped_pool_boxes.insert(((j + 1)*4)+i, pool_boxes[conf][position][i][j])
+                        brn = j % 4
+                        # x = j+((cids[2]//4)*8)+(((cids[2]*cids[3])//96)*24)
+                        x = j+((cids[2]//4)*8)
+                        y = col_idx + ((1+i) if (((cids[1] + 1) % 3) == 0) else 0)
+                        # print(f"\t{cids=}, {x=}, {y=}, {i=}, {j=}, {col_idx=}, {brn=}, {(j*4)+i+(brn*8)=}, {(i*6)+j=}, {text=}")
+                        print(f"\t{cids=}, {x=}, {y=}, {i=}, {j=}, {col_idx=}, {brn=}, {text=}")
+                        # prepped_pool_boxes.insert((j*4)+i, pool_boxes[conf][position][i][j])
+                        # # prepped_pool_boxes.insert(, pool_boxes[conf][position][i][j])
+                        ordered_players.append(text)
+                        # print(f'AAA = {prepped_pool_boxes[x]=}, {prepped_pool_boxes[x][y]=}')
+                        prepped_pool_boxes[x][y] = text
+                        cids[3] += 1
+                    # if len(box) == 6:
+                    #     prepped_pool_boxes[x+1][y] = None
+                    #     prepped_pool_boxes[x+2][y] = None
+                    # if len(box) == 6:
+                    #     ordered_players.append(None)
+                    #     ordered_players.append(None)
+                    cids[2] += 1
+                cids[1] += 1
+            cids[0] += 1
+        # # print(f"{len(ordered_players)=}, {ordered_players=}")
+        #
+        # # grouped = [ordered_players[i:i + 8] for i in range(0, len(ordered_players), 8)]
+        #
+        # for box in ordered_players[::8]:
+        #     print(f"\t{box=}")
 
-        p_conf_boxes = []
-        row_boxes = []
-
-        # # Top row West
-        # prepped_pool_boxes += [f"East - Forward {i} (pick 1)" for i in range(1, 5)]
-        # for j in range(4):
-        #     for i in range(6):
-        #         print(f"ROW1 ({i}, {j}) {((j + 1)*4)+i}, {(i*6)+j} = {pool_boxes['W']['F'][i][j]=}")
-        #         # prepped_pool_boxes.append(pool_boxes['W']['F'][i][j])
-        #         prepped_pool_boxes.insert((i*6)+j, pool_boxes['W']['F'][i][j])
-
-        # space row
-        prepped_pool_boxes += [" "] * 4
-
-        # # 2nd row West
-        # prepped_pool_boxes += [f"East - Forward {i} (pick 1)" for i in range(5, 9)]
-        # for j in range(4, 8):
-        #     for i in range(6):
-        #         print(f"ROW2 ({i}, {j}) = {pool_boxes['W']['F'][i][j]=}")
-        #         prepped_pool_boxes.append(pool_boxes['W']['F'][i][j])
-
-        print(f"{prepped_pool_boxes=}")
-
-        # Generate the PDF
-        generate_pdf(prepped_pool_boxes, title)
+        # print(f"{ordered_players=}")
+        # players_by_row = []
+        # for i in range(6):
+        #     # prepped_pool_boxes += ordered_players[::6+i] + [""] * 4
+        #     x = len(ordered_players[i])
+        #     players_by_row += [ordered_players[i::x]]
+        #     print(f"{i=}, {x=}, {ordered_players[i::x]=}")
+        #
+        # print(f"{len(players_by_row)=}, {players_by_row=}")
+        # zip_lsts = [[None]*8]*len(players_by_row)
+        # zip_lsts += players_by_row
+        #
+        # for a, b, c, d, e, f, *g in zip(*zip_lsts):
+        # # for a, b, c, d, *e in zip(*players_by_row):
+        #     print(f"{a=}, {b=}, {c=}, {d=}, {e=}, {f=}, {g=}")
+        #     # print(f"{a=}, {b=}, {c=}, {d=}, {e=}")
+        #
+        # prepped_pool_boxes = prepped_pool_boxes[:-4]
+        #
+        # p_conf_boxes = []
+        # row_boxes = []
+        #
+        # # # # Top row West
+        # # # prepped_pool_boxes += [f"East - Forward {i} (pick 1)" for i in range(1, 5)]
+        # # # for j in range(4):
+        # # #     for i in range(6):
+        # # #         print(f"ROW1 ({i}, {j}) {((j + 1)*4)+i}, {(i*6)+j} = {pool_boxes['W']['F'][i][j]=}")
+        # # #         # prepped_pool_boxes.append(pool_boxes['W']['F'][i][j])
+        # # #         prepped_pool_boxes.insert((i*6)+j, pool_boxes['W']['F'][i][j])
+        # #
+        # # # space row
+        # # prepped_pool_boxes += [" "] * 4
+        # #
+        # # # # 2nd row West
+        # # # prepped_pool_boxes += [f"East - Forward {i} (pick 1)" for i in range(5, 9)]
+        # # # for j in range(4, 8):
+        # # #     for i in range(6):
+        # # #         print(f"ROW2 ({i}, {j}) = {pool_boxes['W']['F'][i][j]=}")
+        # # #         prepped_pool_boxes.append(pool_boxes['W']['F'][i][j])
+        #
+        print(f"\n\n\tEND\n{prepped_pool_boxes=}")
+        for v in prepped_pool_boxes:
+            print(f"lv={len(v)}, {v=}")
+        #
+        # # Generate the PDF
+        # generate_pdf(prepped_pool_boxes, title)
 
     # test_3()
     my_version_1()
