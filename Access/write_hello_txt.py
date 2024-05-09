7ca3364d152ad178e926c0cfdb291f5b26549a83
Avery Briggs <abriggs1@unb.ca>
2024-05-08 21:09:03 -0300

    2024-05-08 Hockey Pool

--- Access/write_hello_txt.py
+++ Access/write_hello_txt.py
@@ -1,12 +1,19 @@
 import datetime
+import os
 
 
 
 #if __name__ == "__main__":
-p1 = "hello.txt"
-p2 = r"C:\Users\abrig\Documents\Coding_Practice\Access\hello.txt"
-with open(p2, "w") as f:
+f = "hello.txt"
+p2 = r"C:\Users\abrig\Documents\Coding_Practice\Access"
+p3 = r"C:\Users\ABriggs\Documents\Coding Practice\Coding_Practice\Access"
+
+p = p3 if os.path.isdir(p3) else p2
+
+with open(f"{p}\{f}", "w") as f:
 	now = datetime.datetime.now()
 	f.write(f"Hello at {now:%Y-%m-%d %H:%M:%S}")
 print(f"program over.")
+
+input("hit enter to quit.")
 		
