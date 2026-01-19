db1360eb648418f72e05868cdb6a2e8ab5dae6d5
Avery <abriggs1@unb.ca>
2026-01-19 03:28:53 -0400

    2026-01-19 GamePredictions

--- Python/TV_series/snake.py
+++ Python/TV_series/snake.py
@@ -1,3 +1,5 @@
+import random
+
 from colour_utility import Colour
 from utility import grid_cells, clamp
 from pygame_utility import pygame, write_text
@@ -32,6 +34,18 @@ def draw_snake():
         h = r_data["h"]
         rect = pygame.Rect(x_1, y_1, w, h)
         pygame.draw.rect(WINDOW, colour_snake.rgb_code, rect)
+
+
+def draw_food():
+    for i, idx in enumerate(food):
+        i_, j_ = idx
+        r_data = gc_cells[i_][j_]
+        x_1 = r_data["x_1"]
+        y_1 = r_data["y_1"]
+        w = r_data["w"]
+        h = r_data["h"]
+        rect = pygame.Rect(x_1, y_1, w, h)
+        pygame.draw.rect(WINDOW, colour_food.rgb_code, rect)
 
 
 def draw_time():
@@ -84,6 +98,28 @@ def move_snake():
         # rect = pygame.Rect(x_1, y_1, w, h)
 
 
+def add_food():
+    cells = [(i, j) for j in range(n_cols) for i in range(n_rows)]
+    print("cells A")
+    print(cells)
+    print("snake A")
+    print(snake)
+    for i, idx in enumerate(snake):
+        i_, j_ = idx
+        cells.remove((i_, j_))
+    for i, idx in enumerate(food):
+        i_, j_ = idx
+        cells.remove((i_, j_))
+    n_to_choose = n_new_food - len(food)
+    print("cells B")
+    print(cells)
+    print(f"{n_to_choose=}")
+    if n_to_choose > 0:
+        chxs = random.sample(cells, k=n_to_choose)
+        print(f"CHOSE {chxs=}")
+        food.extend(chxs)
+
+
 if __name__ == "__main__":
     pygame.init()
     WIDTH, HEIGHT = 750, 550
@@ -110,6 +146,7 @@ if __name__ == "__main__":
     snake = [(n_rows//2, n_cols//2)]
     snake_direction = 1, 0
     food = []
+    eaten_food = []
     n_new_food = 1
 
     gc_cells = grid_cells(
@@ -142,9 +179,10 @@ if __name__ == "__main__":
     colour_mallow = Colour((0, 120, 250))
     bg_grid_cell = Colour((160, 160, 160))
     colour_snake = Colour((10, 210, 40))
+    colour_food = Colour((210, 40, 10))
 
     while running:
-        CLOCK.tick(FPS_START)
+        CLOCK.tick(FPS_START + (len(snake)))
         time_passed += CLOCK.get_time()
         ticks_passed += 1
 
@@ -158,9 +196,10 @@ if __name__ == "__main__":
         # WINDOW.blit(text_surface, text_rect)
 
         if len(food) != n_new_food:
-            
+            add_food()
 
         draw_grid()
+        draw_food()
         draw_snake()
         draw_time()
         draw_score()
@@ -187,39 +226,79 @@ if __name__ == "__main__":
                 if event.key == pygame.K_DOWN:
                     y_accel = y_accel_r
                     snake_direction = (0, 1)
-            elif event.type == pygame.KEYUP:
-                if event.key in (pygame.K_LEFT, pygame.K_RIGHT):
-                    x_accel = 0
-                if event.key in (pygame.K_UP, pygame.K_DOWN):
-                    y_accel = 0
+            # elif event.type == pygame.KEYUP:
+            #     if event.key in (pygame.K_LEFT, pygame.K_RIGHT):
+            #         x_accel = 0
+            #     if event.key in (pygame.K_UP, pygame.K_DOWN):
+            #         y_accel = 0
 
-        x_change += x_accel  # Accelerate.
-        y_change += y_accel  # Accelerate.
-        if abs(x_change) >= max_speed:  # If max_speed is exceeded.
-            # Normalize the x_change and multiply it with the max_speed.
-            x_change = x_change / abs(x_change) * max_speed
-        if abs(y_change) >= max_speed:  # If max_speed is exceeded.
-            # Normalize the x_change and multiply it with the max_speed.
-            y_change = y_change / abs(y_change) * max_speed
+        snake_head = snake[0]
+        food_eaten = []
+        print(f"{food=}")
+        for i, idx in enumerate(food):
+            i_, j_ = idx
+            if snake_head == idx:
+                print(f"eat {i=}, {idx=}, {snake_head=}")
+                food_eaten.append(i)
+        for idx in food_eaten:
+            # [i, j, snake_len]
+            eaten_food.append((*food[idx], len(snake)+1))
+            food.pop(idx)
+        if eaten_food:
+            n_eaten_food = []
+            for i_, j_, l_snake in eaten_food:
+                l_snake -= 1
+                if l_snake < 0:
+                    snake.append((i_, j_))
+                else:
+                    n_eaten_food.append((i_, j_, l_snake))
+            eaten_food = n_eaten_food
 
-        # Decelerate if no key is pressed.
-        if x_accel == 0:
-            x_change *= x_decel_r
-        if y_accel == 0:
-            y_change *= y_decel_r
+        print(f"A {snake=}, {set(snake)=}")
+        print(f"B {eaten_food=}, {set(eaten_food)=}")
+        # s_snake = set(snake)
+        # s_snake.union(set(eaten_food))
+        # print(f"B {set(eaten_food)=}")
+        # print(f"C {len(s_snake)=}")
+        # print(f"D {s_snake=}")
+        # print(f"E {len(snake)=} {len(eaten_food)=}")
+        ss = [f"{idx[0]}_{idx[1]}" for idx in snake]
+        sef = [f"{idx[0]}_{idx[1]}" for idx in eaten_food]
+        print(f"S={ss}, SEF={sef}")
+        # if (len(set(ss)) + len(set(sef))) != (len(snake) + len(eaten_food)):
+        #     print(f"Game over")
+        #     running = False
 
-        # x += x_change  # Move the object.
-        win_rect = WINDOW.get_rect()
-        x = clamp(win_rect.left + (m_width / 2), x + x_change, win_rect.right - (m_width / 2))  # Move the object.
-        y = clamp(win_rect.top + (m_height / 2), y + y_change, win_rect.bottom - (m_height / 2))  # Move the object.
 
-        mallow_rect.center = x, y
-        pygame.draw.rect(WINDOW, colour_mallow.rgb_code, mallow_rect)
 
-        # update the display
-        # draw everything
-        # pygame.display.flip()
-        # draw everything, or pass a surface or shape to update only that portion.
+
+        # x_change += x_accel  # Accelerate.
+        # y_change += y_accel  # Accelerate.
+        # if abs(x_change) >= max_speed:  # If max_speed is exceeded.
+        #     # Normalize the x_change and multiply it with the max_speed.
+        #     x_change = x_change / abs(x_change) * max_speed
+        # if abs(y_change) >= max_speed:  # If max_speed is exceeded.
+        #     # Normalize the x_change and multiply it with the max_speed.
+        #     y_change = y_change / abs(y_change) * max_speed
+        #
+        # # Decelerate if no key is pressed.
+        # if x_accel == 0:
+        #     x_change *= x_decel_r
+        # if y_accel == 0:
+        #     y_change *= y_decel_r
+        #
+        # # x += x_change  # Move the object.
+        # win_rect = WINDOW.get_rect()
+        # x = clamp(win_rect.left + (m_width / 2), x + x_change, win_rect.right - (m_width / 2))  # Move the object.
+        # y = clamp(win_rect.top + (m_height / 2), y + y_change, win_rect.bottom - (m_height / 2))  # Move the object.
+        #
+        # mallow_rect.center = x, y
+        # pygame.draw.rect(WINDOW, colour_mallow.rgb_code, mallow_rect)
+        #
+        # # update the display
+        # # draw everything
+        # # pygame.display.flip()
+        # # draw everything, or pass a surface or shape to update only that portion.
         pygame.display.update()
 
     pygame.quit()
